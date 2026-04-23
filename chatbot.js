a3f9a6c62c6c325e4f52fe2944b6443fb45cec75
alexandreTIMAL <alexandre.timal@epitech.eu>
2026-04-23 10:53:48 +0200

    feat: adding lab8

---
+++ chatbot.js
@@ -0,0 +1,81 @@
+let questions = [
+    {
+        question: "Do you feel happy?",
+        options: {
+            a: "Yes",
+            b: "No"
+        },
+        correctAnswer: "a",
+        correctResponse: "Super!",
+        incorrectResponse: "I am sorry"
+    },
+    {
+        question: "Do you like web design?",
+        options: {
+            a: "Yes",
+            b: "No"
+        },
+        correctAnswer: "a",
+        correctResponse: "Great, me too!",
+        incorrectResponse: "That's too bad..."
+    },
+    {
+        question: "Is JavaScript fun?",
+        options: {
+            a: "Yes",
+            b: "No"
+        },
+        correctAnswer: "a",
+        correctResponse: "Awesome!",
+        incorrectResponse: "Give it another chance!"
+    }
+];
+
+let currentQuestionIndex = 0;
+let chatContainer = document.getElementById("chat-container");
+let chatForm = document.getElementById("chat-form");
+let userInput = document.getElementById("user-input");
+displayQuestion();
+
+function displayQuestion() {
+    let currentQuestion = questions[currentQuestionIndex];
+    let optionsHTML = Object.keys(currentQuestion.options).map(key => `${key}. ${currentQuestion.options[key]}`).join(' ');
+
+    let botResponse = document.createElement("div");
+    botResponse.classList.add("message");
+    botResponse.innerHTML = `<strong>Bot:</strong> ${currentQuestion.question} ${optionsHTML}`;
+    chatContainer.appendChild(botResponse);
+}
+
+function scrollChatContainerToBottom() {
+    let chatContainer = document.getElementById("chat-container");
+    chatContainer.scrollTop = chatContainer.scrollHeight;
+}
+
+chatForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    let userResponse = userInput.value.toLowerCase();
+
+    let userMessage = document.createElement("div");
+    userMessage.classList.add("message");
+    userMessage.innerHTML = `<strong>You :</strong> ${userResponse}`;
+    chatContainer.appendChild(userMessage);
+
+    let currentQuestion = questions[currentQuestionIndex];
+    let botResponse = document.createElement("div");
+    botResponse.classList.add("message");
+    botResponse.innerHTML = `<strong>Bot :</strong> `;
+    if (userResponse === currentQuestion.correctAnswer) {
+        botResponse.innerHTML += currentQuestion.correctResponse;
+    } else {
+        botResponse.innerHTML += currentQuestion.incorrectResponse;
+    }
+    chatContainer.appendChild(botResponse);
+
+    currentQuestionIndex = (currentQuestionIndex + 1) % questions.length;
+    userInput.value = "";
+    displayQuestion();
+
+    scrollChatContainerToBottom();
+});
